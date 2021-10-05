0f579b779306b80e3c493b46532604bafa0df789
Aaron Ross <superhawk610@gmail.com>
2021-10-05 01:25:03 -0700

    wip: repl history

--- src/repl.c
+++ src/repl.c
@@ -4,12 +4,66 @@
 #include "repl.h"
 #include "vm.h"
 
-// TODO: Track command history (readline has some built-ins for this)
-// TODO: Tab completion (readline should be of some help here as well)
+// TODO: Track command history
+// TODO: Tab completion
 // https://web.mit.edu/gnu/doc/html/rlman_2.html
 
-static int handle_up_arrow(int count, int key) { return 0; }
-static int handle_down_arrow(int count, int key) { return 0; }
+typedef struct History {
+  char* line;
+
+  struct History* prev;
+  struct History* next;
+} History;
+
+History* history = NULL;
+
+static char* history_prev() {
+  if (history == NULL || history->prev == NULL) return NULL;
+
+  history = history->prev;
+  return history->line;
+}
+
+static char* history_next() {
+  if (history == NULL || history->next == NULL) return NULL;
+
+  history = history->next;
+  return history->line;
+}
+
+static void history_push(char* line) {
+  if (history->next != NULL) {
+    // TODO: We're rewriting history, so free anything that
+    //       will no longer be reachable.
+  }
+
+  History* new_history = (History*) malloc(sizeof(History));
+  history->next = new_history;
+  new_history->prev = history;
+  history = new_history;
+}
+
+static void history_free() {
+  // TODO: Walk back to front of list, then walk forward and free
+  //       as we go.
+}
+
+static int handle_up_arrow(int count, int key) {
+  char* prev = history_prev();
+  if (prev == NULL) return 0;
+
+  rl_replace_line(prev, 0);
+  return 0;
+}
+
+static int handle_down_arrow(int count, int key) {
+  char* next = history_next();
+  if (next == NULL) return 0;
+
+  rl_replace_line(next, 0);
+  return 0;
+}
+
 static int handle_tab(int count, int key) { return 0; }
 
 void repl() {
@@ -23,7 +77,9 @@ void repl() {
     line = readline("> ");
     if (!line) break; // break on EOF
 
+    history_push(line);
     interpret(line);
-    free(line);
   }
+
+  history_free();
 }
